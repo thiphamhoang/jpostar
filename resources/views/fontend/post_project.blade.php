c25deae2d94d11d6864cc8a1e675ccd0da67243f
thiphamhoang@gmail.com <thiphamhoang@gmail.com>
2025-08-14 14:23:16 +0700

    df

--- resources/views/fontend/post_project.blade.php
+++ resources/views/fontend/post_project.blade.php
@@ -41,7 +41,7 @@
                         <div class="msolutions-descr"> <?php echo html_entity_decode($post->da_tinhNang); ?></div>
                     </div>
 
-                    <div class=" wow fadeInUp" style="visibility: visible; animation-name: fadeInUp; text-align: center; width: 100%;">
+                    <div class="paidmain-content-img  wow fadeInUp" style="visibility: visible; animation-name: fadeInUp; text-align: center;">
                         @if($post->img)
 
 
